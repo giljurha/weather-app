185b2725ccde4f24e8bad0b83ea12e86c3435854
DESKTOP-2IJ6G78\gilju <giljurha93@gmail.com>
2024-08-18 02:04:33 +0900

    프로잭트 생성

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { useEffect, useState } from 'react';
+import WeatherBox from './component/WeatherBox';
+import WeatherButton from './component/WeatherButton';
+
+function App() {
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeatherByCurrentLocation(lat, lon);
+    })
+  }
+
+  const getWeatherByCurrentLocation = async(lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a58ba93b4908f9c534f17f4a32b54cc3`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log("data", data);
+  }
+  useEffect(() => {
+    getCurrentLocation()
+  }, [])
+
+  return (
+    <div>
+      <div class="container">
+        <WeatherBox />
+        <WeatherButton />
+      </div>
+    </div>
+  );
+}
+
+export default App;
